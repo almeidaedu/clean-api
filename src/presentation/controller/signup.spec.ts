29cad1560a5b97dc7daf820c2f7ce255e99855ea
Eduardo Almeida <eduardo.hpa87@gmail.com>
2024-08-30 13:47:48 -0300

    refactor: ensure SignUpController use types and body interface without  type casting

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,10 +1,26 @@
 import { SignUpController } from './signup'
 
 describe('Signup Controller', () => {
+  test('should return 400 if request body is invalid', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        user: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: undefined
+
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Invalid request body'))
+  })
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
+        name: null,
         email: 'any_email@email.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
@@ -19,6 +35,7 @@ describe('Signup Controller', () => {
     const httpRequest = {
       body: {
         name: 'any_name',
+        email: null,
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,15 +1,55 @@
+import type { HttpRequest, HttpResponse } from '../../presentation/protocols/http'
+
+interface ISignUpRequestBody {
+  name: string
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+function isSignUpRequestBody (body: unknown): body is ISignUpRequestBody {
+  if (body !== null && typeof body === 'object') {
+    return 'name' in body &&
+    'email' in body &&
+    'password' in body &&
+    'passwordConfirmation' in body
+  }
+
+  return false
+}
+
 export class SignUpController {
-  handle (httpRequest: any): any {
-    if (!httpRequest.body.name) {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const { body } = httpRequest
+
+    if (!isSignUpRequestBody(body)) {
+      return {
+        statusCode: 400,
+        body: new Error('Invalid request body')
+      }
+    }
+
+    const { name, email } = body
+
+    if (!name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
       }
     }
-    if (!httpRequest.body.email) {
+
+    if (!email) {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
+      }
+    }
+
+    return {
+      statusCode: 200,
+      body: {
+        name,
+        email
       }
     }
   }
