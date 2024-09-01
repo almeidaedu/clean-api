d9f14b6380eb6e93fb0208932b9eb05aaef0fb4b
Eduardo Almeida <eduardo.hpa87@gmail.com>
2024-09-01 12:22:43 -0300

    refactor: Add password confirmation validation in SignUpController

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -41,6 +41,10 @@ export class SignUpController implements Controller {
         }
       }
 
+      if (body.password !== body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
       const isValid = this.emailValidator.isValid(body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
