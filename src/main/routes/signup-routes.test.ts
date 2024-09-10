20dce42d2337fed2689d888d19520d22bca89d7a
Eduardo Almeida <eduardo.hpa87@gmail.com>
2024-09-10 11:34:50 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,25 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    const mongoConnString = process.env.MONGO_URL
+    if (!mongoConnString) {
+      throw new Error('MONGO_URL not set in env')
+    }
+    await MongoHelper.connect(mongoConnString)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should an account on success', async () => {
     await request(app)
       .post('/api/signup')
