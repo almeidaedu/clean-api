e9d2e4437b5216ea171c3246a20ce46b909ca14d
Eduardo Almeida <eduardo.hpa87@gmail.com>
2024-09-21 14:07:13 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log-spec.ts
@@ -0,0 +1,30 @@
+import type { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+  beforeAll(async () => {
+    const mongoConnString = process.env.MONGO_URL
+    if (!mongoConnString) {
+      throw new Error('MONGO_URL not set in env')
+    }
+    await MongoHelper.connect(mongoConnString)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
