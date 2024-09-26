0eee22eaec0532ebbd04f0e3704ef7d4151d58ff
Eduardo Almeida <eduardo.hpa87@gmail.com>
2024-09-26 11:55:34 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import type { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import type { EmailValidator } from '../../protocols/email-validator'
 
@@ -25,26 +25,34 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['email', 'password']
+    try {
+      const requiredFields = ['email', 'password']
 
-    if (!isLoginRequestBody(httpRequest.body)) {
-      return badRequest(new InvalidParamError(requiredFields.join(', ')))
-    }
+      if (!isLoginRequestBody(httpRequest.body)) {
+        return badRequest(new InvalidParamError(requiredFields.join(', ')))
+      }
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      const { email } = httpRequest.body
+
+      const isValid = this.emailValidator.isValid(email)
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      return await new Promise(resolve => { resolve({ statusCode: 200, body: {} }) })
+    } catch (error) {
+      if (error instanceof Error) {
+        return serverError(error)
+      } else {
+        return serverError(new Error('Unknown error'))
       }
     }
-
-    const { email } = httpRequest.body
-
-    const isValid = this.emailValidator.isValid(email)
-
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    return await new Promise(resolve => { resolve({ statusCode: 200, body: {} }) })
   }
 }
