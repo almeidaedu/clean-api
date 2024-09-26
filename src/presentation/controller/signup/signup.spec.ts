5956dae014160bc28a4c313377502287abcde635
Eduardo Almeida <eduardo.hpa87@gmail.com>
2024-09-26 15:13:13 -0300

    test: ensure SignupController returns 400 if Validation fails

--- src/presentation/controller/signup/signup.spec.ts
+++ src/presentation/controller/signup/signup.spec.ts
@@ -15,8 +15,8 @@ const makeEmailValidator = (): EmailValidator => {
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
-    validate (input: unknown): Error {
-      return new Error()
+    validate (input: any): Error | null {
+      return null
     }
   }
 
@@ -212,5 +212,12 @@ describe('Signup Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
